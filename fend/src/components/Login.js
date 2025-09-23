d51a33e28a781f4be2d7da305a23e034835e4527
Bandasumanth <162335926+Bandasumanth@users.noreply.github.com>
2025-09-23 11:09:59 +0530

    Update Login.js

--- fend/src/components/Login.js
+++ fend/src/components/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', formData);
+      const res = await axios.post('https://foodmong-bendd.onrender.com/api/auth/login', formData);
       login(res.data.token);
       navigate('/');
     } catch (err) {
